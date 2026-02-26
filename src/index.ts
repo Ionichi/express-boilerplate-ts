b03e1a3443f90d9a363ff94b2e1f59281a1e5214
ionichi <feryandi2303@gmail.com>
2026-02-26 16:07:09 +0700

    update: package update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Your code here");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello World!");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
